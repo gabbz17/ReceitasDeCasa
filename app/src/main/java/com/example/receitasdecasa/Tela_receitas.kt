fc208b1de8dffc4844f003fbf99d5163980d2703
Gabriel Coutinho <coutinho.dev17@gmail.com>
2024-11-25 20:38:48 -0300

    finalizando interface do app

--- app/src/main/java/com/example/receitasdecasa/Tela_receitas.kt
+++ app/src/main/java/com/example/receitasdecasa/Tela_receitas.kt
@@ -52,5 +52,95 @@ class Tela_receitas : AppCompatActivity() {
         binding.foto2.setOnClickListener {
             startActivity(Intent(this, Tela_frango::class.java))
         }
+        binding.back3.setOnClickListener {
+            startActivity(Intent(this, Tela_molho::class.java))
+        }
+        binding.sb3.setOnClickListener {
+            startActivity(Intent(this, Tela_molho::class.java))
+        }
+        binding.sbt3.setOnClickListener {
+            startActivity(Intent(this, Tela_molho::class.java))
+        }
+        binding.title3.setOnClickListener {
+            startActivity(Intent(this, Tela_molho::class.java))
+        }
+        binding.foto3.setOnClickListener {
+            startActivity(Intent(this, Tela_molho::class.java))
+        }
+        binding.back4.setOnClickListener {
+            startActivity(Intent(this, Tela_bolo::class.java))
+        }
+        binding.sb4.setOnClickListener {
+            startActivity(Intent(this, Tela_bolo::class.java))
+        }
+        binding.sbt4.setOnClickListener {
+            startActivity(Intent(this, Tela_bolo::class.java))
+        }
+        binding.title4.setOnClickListener {
+            startActivity(Intent(this, Tela_bolo::class.java))
+        }
+        binding.foto4.setOnClickListener {
+            startActivity(Intent(this, Tela_bolo::class.java))
+        }
+        binding.back5.setOnClickListener {
+            startActivity(Intent(this, Tela_mamao::class.java))
+        }
+        binding.sb5.setOnClickListener {
+            startActivity(Intent(this, Tela_mamao::class.java))
+        }
+        binding.sbt5.setOnClickListener {
+            startActivity(Intent(this, Tela_mamao::class.java))
+        }
+        binding.title5.setOnClickListener {
+            startActivity(Intent(this, Tela_mamao::class.java))
+        }
+        binding.foto5.setOnClickListener {
+            startActivity(Intent(this, Tela_mamao::class.java))
+        }
+        binding.back6.setOnClickListener {
+            startActivity(Intent(this, Tela_picao::class.java))
+        }
+        binding.sb6.setOnClickListener {
+            startActivity(Intent(this, Tela_picao::class.java))
+        }
+        binding.sbt6.setOnClickListener {
+            startActivity(Intent(this, Tela_picao::class.java))
+        }
+        binding.title6.setOnClickListener {
+            startActivity(Intent(this, Tela_picao::class.java))
+        }
+        binding.foto6.setOnClickListener {
+            startActivity(Intent(this, Tela_picao::class.java))
+        }
+        binding.back7.setOnClickListener {
+            startActivity(Intent(this, Tela_tortilha::class.java))
+        }
+        binding.sb7.setOnClickListener {
+            startActivity(Intent(this, Tela_tortilha::class.java))
+        }
+        binding.sbt7.setOnClickListener {
+            startActivity(Intent(this, Tela_tortilha::class.java))
+        }
+        binding.title7.setOnClickListener {
+            startActivity(Intent(this, Tela_tortilha::class.java))
+        }
+        binding.foto7.setOnClickListener {
+            startActivity(Intent(this, Tela_tortilha::class.java))
+        }
+        binding.back8.setOnClickListener {
+            startActivity(Intent(this, Tela_panqueca::class.java))
+        }
+        binding.sb8.setOnClickListener {
+            startActivity(Intent(this, Tela_panqueca::class.java))
+        }
+        binding.sbt8.setOnClickListener {
+            startActivity(Intent(this, Tela_panqueca::class.java))
+        }
+        binding.title8.setOnClickListener {
+            startActivity(Intent(this, Tela_panqueca::class.java))
+        }
+        binding.foto8.setOnClickListener {
+            startActivity(Intent(this, Tela_panqueca::class.java))
+        }
     }
 }
